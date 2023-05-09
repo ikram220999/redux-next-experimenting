1b36fb6a903788bdb53c5446053f020dd63e150a
ikram dev <lalatputih22@gmail.com>
2023-05-09 23:54:07 +0800

    first commit

--- src/pages/api/counter.ts
+++ src/pages/api/counter.ts
@@ -4,7 +4,7 @@ const countHandler: NextApiHandler = async (request, response) => {
   const { amount = 1 } = request.body
 
   // simulate IO latency
-  await new Promise((resolve) => setTimeout(resolve, 500))
+  await new Promise((resolve) => setTimeout(resolve, 100))
 
   response.json({ data: amount })
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const IndexPage: NextPage = () => {
+  return (
+    <div className=''>
+      <Head>
+        <title>Redux Toolkit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      anak kambing
+    </div>
+  )
+}
+
+export default IndexPage
